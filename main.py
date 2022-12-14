1f00c3b6a12f42beb6c5496c5e8a424f8746f4d0
Namrata Dhobale <64163259+ngdhobale@users.noreply.github.com>
2022-12-14 19:26:49 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+import streamlit as st
+import streamlit.components.v1 as components
+
+# >>> import plotly.express as px
+# >>> fig = px.box(range(10))
+# >>> fig.write_html('test.html')
+
+st.header("test html import")
+
+HtmlFile = open("home.html", 'r', encoding='utf-8')
+source_code = HtmlFile.read() 
+print(source_code)
+components.html(source_code)
