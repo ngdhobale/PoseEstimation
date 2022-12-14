de1864a605079787bb0462f578ee938e9866cd85
Namrata Dhobale <64163259+ngdhobale@users.noreply.github.com>
2022-12-14 19:28:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import streamlit.components.v1 as components
 
 st.header("test html import")
 
-HtmlFile = open("home.html", 'r', encoding='utf-8')
+HtmlFile = open("index.html", 'r', encoding='utf-8')
 source_code = HtmlFile.read() 
 print(source_code)
 components.html(source_code)
