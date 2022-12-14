c1bd0bb02430b4ae6ca73218eb87052d62d3d2d9
Namrata Dhobale <64163259+ngdhobale@users.noreply.github.com>
2022-12-14 19:32:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,6 @@ Write your code in this editor and press "Run" button to execute it.
 import streamlit as st
 import streamlit.components.v1 as components
 
-# >>> import plotly.express as px
-# >>> fig = px.box(range(10))
-# >>> fig.write_html('test.html')
-
 st.header("test html import")
 
 HtmlFile = open("index.html", 'r', encoding='utf-8')
